45ff116d913c012857a84a0788dc34aa04b4fe79
Samir210-az <samir.akhundoff@gmail.com>
2026-06-10 03:21:20 +0400

    Kes menyu-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "menyu-v3";
+const CACHE = "menyu-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
