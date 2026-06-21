7aba4379d3127a58ba934d1276827f2ddf6cd736
Samir210-az <samir.akhundoff@gmail.com>
2026-06-21 21:46:35 +0400

    Remove login system

--- customers-data.js
+++
@@ -1,6 +0,0 @@
-/* ============================================================
-   AN Psixoloji — Müştəri Bazası (customers-data.js)
-   Admin panel bu faylı yeniləyir
-============================================================ */
-window.CUSTOMERS_DB = {
-};
