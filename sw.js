f1c2160a11b4bc2f97dd9c6287a2ad89111a48b6
Samir210-az <samir.akhundoff@gmail.com>
2026-07-04 12:02:38 +0400

    Bump cache version to v5 for parallax update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "menyu-v4";
+const CACHE = "menyu-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
